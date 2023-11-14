9df5d70a406a2dd60bf737cd110f882e64abe0f4
Jorge Henao <jhneao@merqueo.com>
2023-11-14 15:21:32 -0500

    feature: HolaflyTestNavigation was integrated to have Hilt injection available

--- app/src/main/java/com/jh/holaflytest/ui/MainActivity.kt
+++ app/src/main/java/com/jh/holaflytest/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jh.holaflytest
+package com.jh.holaflytest.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jh.holaflytest.ui.navigation.HolaflyTestNavigation
 import com.jh.holaflytest.ui.theme.HolaflyTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/jh/holaflytest/ui/navigation/HolaflyTestNavigation.kt
+++ app/src/main/java/com/jh/holaflytest/ui/navigation/HolaflyTestNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.jh.holaflytest.ui.screens.SplashScreen
 
 /**
  * Created by Jorge Henao on 13/11/23.
@@ -17,7 +18,9 @@ fun HolaflyTestNavigation() {
         startDestination = Screens.SplashScreen.name
     ) {
         composable(Screens.SplashScreen.name) {
-
+            SplashScreen { route ->
+                navController.navigate(route)
+            }
         }
         composable(Screens.HomeScreen.name) {
 
